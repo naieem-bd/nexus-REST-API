c666462c7124b621f800d6ae381545e5eacef797
naieem-bd <naieemur@gmail.com>
2022-08-09 12:10:51 +0600

    initial server setup and connect to the MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log('Connect to MongoDB');
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('hi from homepage');
+});
 
 app.listen(2727, () => {
-  console.log('hello node');
+  console.log('hello node JS');
 });
