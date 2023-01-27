775d9aca7cc59cfaa2582686a5a11a1abf988ed4
naieem-bd <naieemur@gmail.com>
2023-01-27 19:58:40 +0600

    update user routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,34 @@
+const User = require('../models/User');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-  res.send('get all users');
+// update user
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json('Profile has been updated');
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json('You can modify your account only!');
+  }
 });
+
+// delete user
+// get a user
+// follow a user
+// unfollow a user
 
 module.exports = router;
