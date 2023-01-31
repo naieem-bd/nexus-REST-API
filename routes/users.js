be5e25b2e05f77f5e83f40025ab3357dfbf220a0
naieem-bd <naieemur@gmail.com>
2023-01-31 07:59:32 +0600

    get a user route added

--- routes/users.js
+++ routes/users.js
@@ -41,6 +41,16 @@ router.delete('/:id', async (req, res) => {
 });
 
 // get a user
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 // follow a user
 // unfollow a user
 
