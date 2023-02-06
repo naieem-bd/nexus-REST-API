7f202a82e6dc5adbe5d66c7c568c845407a824ea
naieem-bd <naieemur@gmail.com>
2023-02-06 12:42:40 +0600

    follow user modified

--- routes/users.js
+++ routes/users.js
@@ -52,6 +52,26 @@ router.get('/:id', async (req, res) => {
 });
 
 // follow a user
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json('User successfully followed!');
+      } else {
+        res.status(403).json('You already follow this user');
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json('You can not follow yourself');
+  }
+});
+
 // unfollow a user
 
 module.exports = router;
