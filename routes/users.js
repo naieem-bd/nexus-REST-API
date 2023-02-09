0641cb01526922c49029ced4a2c1662dbc6a44c0
naieem-bd <naieemur@gmail.com>
2023-02-09 17:03:36 +0600

    unfollow route added

--- routes/users.js
+++ routes/users.js
@@ -73,5 +73,24 @@ router.put('/:id/follow', async (req, res) => {
 });
 
 // unfollow a user
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json('User successfully unfollowed!');
+      } else {
+        res.status(403).json('You already unfollow this user');
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json('You can not unfollow yourself');
+  }
+});
 
 module.exports = router;
