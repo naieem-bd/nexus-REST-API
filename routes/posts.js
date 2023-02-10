2400caec241b0fea6e6545b989f40ee653b02188
naieem-bd <naieemur@gmail.com>
2023-02-11 00:36:45 +0600

    get all timeline post route added

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 // create a post
 router.post('/', async (req, res) => {
@@ -48,6 +49,22 @@ router.get('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get timeline posts
+router.get('/timeline/:userId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.params.userId);
+    const currentUserPost = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.status(200).json(currentUserPost.concat(...friendPosts));
   } catch (error) {
     res.status(500).json(error);
   }
