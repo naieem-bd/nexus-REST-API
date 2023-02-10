785755b89c9b86d2dcde407be068b74f40a1f378
naieem-bd <naieemur@gmail.com>
2023-02-10 10:39:50 +0600

    update post route added

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,19 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('The post has been updated');
+    } else {
+      res.status(403).json('You can update only your post');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
