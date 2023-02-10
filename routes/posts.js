75de603728f825626b7cf07514de2d4d57b7f03e
naieem-bd <naieemur@gmail.com>
2023-02-11 01:06:11 +0600

    get user all posts route added

--- routes/posts.js
+++ routes/posts.js
@@ -70,4 +70,19 @@ router.get('/timeline/:userId', async (req, res) => {
   }
 });
 
+// get user's all posts
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) {
+      res.status(404).json('User not found');
+    } else {
+      const posts = await Post.find({ userId: user._id });
+      res.status(200).json(posts);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
