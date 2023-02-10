9fe0c14564e7d4160cb507722393941a2578d78c
naieem-bd <naieemur@gmail.com>
2023-02-10 11:23:42 +0600

    like or dislike post route added

--- routes/posts.js
+++ routes/posts.js
@@ -27,4 +27,20 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// like/dislike a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json('The post has been liked');
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json('The post has been disliked');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
