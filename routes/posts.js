80cf4b2e6483c3dbc811c0f5c4d413b52bd10b6e
naieem-bd <naieemur@gmail.com>
2023-02-11 11:41:38 +0600

    bug fix and fine-tuning

--- routes/posts.js
+++ routes/posts.js
@@ -19,7 +19,7 @@ router.put('/:id', async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
       await post.updateOne({ $set: req.body });
-      res.status(200).json('The post has been updated');
+      res.status(200).json('The post updated');
     } else {
       res.status(403).json('You can update only your post');
     }
@@ -34,10 +34,10 @@ router.put('/:id/like', async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json('The post has been liked');
+      res.status(200).json('The post liked');
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json('The post has been disliked');
+      res.status(200).json('The post disliked');
     }
   } catch (error) {
     res.status(500).json(error);
@@ -70,7 +70,7 @@ router.get('/timeline/:userId', async (req, res) => {
   }
 });
 
-// get user's all posts
+// get a user's all posts
 router.get('/profile/:username', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username });
